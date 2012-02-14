dcd1a86b63b8b8a44e865fc5a3b3fd0e72e3d623
Richard Kent Jordan <rjordan@pobox.com>
2012-02-14 01:13:06 -0600

    Fixed to add filename to grid path

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Filepile2::Application.routes.draw do
-  mount Rack::GridFS::Endpoint.new(:db => Mongoid.database), :at => "gridfs"
+
+  #match "/gridfs/:id/:filename" => Rack::GridFS(:id)
+  mount Rack::GridFS::Endpoint.new(:db => Mongoid.database, :mapper => lambda { |path| %r!/(.+)/!.match(path)[1] }), :at => "/gridfs"
 
   resources :files do
-    member do
-      get :details
-    end
-    collection do
-      post :add_tag
-      post :remove_tag
-      post :clear_tags
-      post :upload
-    end
     resources :tags
   end
 
